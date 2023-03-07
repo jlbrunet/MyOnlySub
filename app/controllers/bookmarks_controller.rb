e5d6920a05480c706ddfa9c8e6b34abee297d0b3
Bastien Lafont <lafont.bastien@gmail.com>
2023-03-07 14:52:27 +0100

    end of methods : add, seen, liked & unliked for Bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
   before_action :look_for_existing_bookmark
+  after_action :book_mark_save
 
 # Démarche commune au trois :
 
@@ -9,36 +10,47 @@ class BookmarksController < ApplicationController
     # si un bookmark, ticked 'false' => 'true' || ticked 'true' => 'false' (sorte de toggle)
 
   def add
-    @bookmark.toggle(:ticked).save
+    @bookmark.toggle(:ticked)
   end
 
   def seen
     if @bookmark.seen = true
       @bookmark.seen = false
       @bookmark.liked = nill
-      @bookmark.save
     else
       @bookmark.seen = true
     end
   end
 
   def liked
-    if @bookmark.liked = nil
+    if @bookmark.liked = nill || @bookmark.liked = false
+      @bookmark.liked = true
+    else
+      @bookmark.liked = nill
+  end
 
+  def unliked
+    if @bookmark.liked = nill || @bookmark.liked = true
+      @bookmark.liked = false
+    else
+      @bookmark.liked = nill
   end
 
   private
 
   def look_for_existing_bookmark
     @movie = Movie.find(params[:movie_id])
-    if Bookmark.where(movie_id: params[:movie_id]).positive?
-      @bookmark = Bookmark.where(movie_id: params[:movie_id])
+    if current_user.bookmarks.where(movie_id: params[:movie_id]).count.positive?
+      @bookmark = current_user.bookmarks.where(movie_id: params[:movie_id])
     else
       @bookmark = Bookmark.new
       @bookmark.movie = @movie
       @bookmark.user = current_user
-      @bookmark.save
     end
+  end
+
+  def book_mark_save
+    @bookmark.save
   end
 
 
