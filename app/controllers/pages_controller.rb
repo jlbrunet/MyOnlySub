cb2ed832ddd4f49ae405be067a9d606cdc1b4faa
Amelie Berthier <amelieberthier.pro@gmail.com>
2023-03-07 14:41:12 +0100

    en cours

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,20 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def recommendation
+  end
+
+  def list
+    @bookmarks = Bookmark.where(user:current_user)
+  end
+
+  def validation
+  end
+
+  def social
+  end
+
+  def sub
+  end
 end
