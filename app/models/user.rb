92176821caa27d1535683664f1a153aa93aee725
Amelie Berthier <amelieberthier.pro@gmail.com>
2023-03-07 12:19:21 +0100

    validations and associations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookmarks, dependent: :destroy
+
+  validates :first_name, presence: :true
+  validates :last_name, presence: :true
+  validates :age, presence: :true, numericality: { only_integer: true }
+  validates :availability, presence: :true, numericality: { only_integer: true }
 end
