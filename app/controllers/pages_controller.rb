98e8a9eb495044d30a02df200213e8aac217fa14
maxco2023 <maxime.coquerelle@gmail.com>
2023-03-09 15:36:40 +0100

    version display search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @movies = Movie.all
     if params[:query].present?
-      @results = Movie.search_by_title_and_synopsis(params[:query])
-    else
-      @movies = Movie.all
-      @bookmark = Bookmark.new
+      @movies = Movie.search_by_title_and_synopsis(params[:query])
+    end
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: "pages/list", locals: {movies: @movies}, formats: [:html] }
     end
   end
 
