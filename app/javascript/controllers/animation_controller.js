777ca5594618e22a88d0415ea662c5e89a5a7792
Julie Brunet <juliebrunet21@msn.com>
2023-03-13 22:25:47 +0100

    ok design à continuer

---
+++ app/javascript/controllers/animation_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="animation"
+export default class extends Controller {
+  static targets = ["platform", "info", "moviesseries", "ibutton"]
+
+  connect() {
+    this.platformTarget.animate([
+      {transform: 'scale(0.4)' },
+     ], {
+      duration: 2000,
+      iteration: Infinity,
+    })
+
+    this.platformTarget.animate( [
+      {transform: 'scale(1)' },
+    ], {
+      duration: 2000,
+      iteration: Infinity,
+    })
+  }
+
+  display() {
+    this.moviesseriesTarget.classList.remove("d-none")
+    this.ibuttonTarget.classList.remove("d-none")
+  }
+
+  i() {
+    this.infoTarget.classList.toggle("d-none")
+  }
+
+}
