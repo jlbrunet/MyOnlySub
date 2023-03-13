9bc14a7405ea31d26e0b4f29459c31400a06e924
Bastien Lafont <lafont.bastien@gmail.com>
2023-03-13 17:43:18 +0100

    end before merging

---
+++ app/javascript/controllers/waiting_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="waiting"
+export default class extends Controller {
+  static targets = ["waiter", "modal"]
+
+
+  waiting(event) {
+    event.preventDefault()
+    this.modalTarget.classList.add('d-none');
+
+    this.waiterTarget.classList.toggle('d-none')
+
+    const form = event.target.closest(".my-form")
+
+
+    setTimeout(() => {
+      form.submit()
+    }, 2800)
+
+  }
+}
