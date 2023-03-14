834fcbac578b61fc54786db6f4096ef0057f8013
Julie Brunet <juliebrunet21@msn.com>
2023-03-14 10:13:50 +0100

    button platform is working with growing and shriking

--- app/javascript/controllers/animation_controller.js
+++ app/javascript/controllers/animation_controller.js
@@ -1,26 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 
+let a = null
 // Connects to data-controller="animation"
 export default class extends Controller {
   static targets = ["platform", "info", "moviesseries", "ibutton"]
 
   connect() {
+    a = setInterval(() => this.animation(), 2000)
+  }
+
+  animation() {
     this.platformTarget.animate([
       {transform: 'scale(0.4)' },
      ], {
-      duration: 2000,
-      iteration: Infinity,
+      duration: 1000,
     })
 
     this.platformTarget.animate( [
       {transform: 'scale(1)' },
     ], {
-      duration: 2000,
-      iteration: Infinity,
+      duration: 1000,
     })
   }
 
   display() {
+    clearInterval(a)
     this.moviesseriesTarget.classList.remove("d-none")
     this.ibuttonTarget.classList.remove("d-none")
   }
